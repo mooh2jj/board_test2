9294d1ba2e554195f30cfcf473cdcd0e3fde8066
mooh2jj@naver.com <62453668+mooh2jj@users.noreply.github.com>
2021-02-23 21:58:36 +0900

    페이징 처리1

--- src/main/java/com/myspring/controller/UserController.java
+++ src/main/java/com/myspring/controller/UserController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.myspring.dto.PagingVO;
 import com.myspring.dto.UserVO;
 
 @Controller
@@ -29,145 +30,171 @@ public class UserController {
 
 	@Autowired
 	SqlSession sqlSession;
-	
+
 	@RequestMapping("/home")
 	public String login() {
-		
+
 		return "member/home";
 	}
-	
+
 	@RequestMapping("/login")
 	public String login(UserVO uvo, HttpServletRequest req, RedirectAttributes rttr, Model model) {
-		
+
 		System.out.println("post login start...");
-		
+
 		HttpSession session = req.getSession();
-		
+
 		UserVO lvo = sqlSession.selectOne("usermapper.login", uvo);
-		
-		if(lvo == null) {
+
+		if (lvo == null) {
 			System.out.println("로그인 실패");
 //			session.setAttribute("member", null);
 //			rttr.addFlashAttribute("msg", false); 이거 안됨
 			model.addAttribute("msg", "false");
 			return "member/home";
-		}else {
+		} else {
 			System.out.println("로그인 성공");
 			session.setAttribute("id", lvo.getId());
 			session.setAttribute("name", lvo.getName());
 			return "member/list";
 		}
-		
+
 	}
-	
+
 	@RequestMapping("/logout")
 	public String logout(HttpServletRequest req, RedirectAttributes rttr, Model model) {
-		
+
 		System.out.println("post logout...");
-		
+
 		req.getSession().invalidate();
 		req.getSession(true);
-		// invalidate()로 현재 사용하고 있는 세션을 무효화한다. 그리고 getSession(true)를 통해서 새로운 세션ID를 발급해준다. 이렇게 처리하여 세션을 초기화
+		// invalidate()로 현재 사용하고 있는 세션을 무효화한다. 그리고 getSession(true)를 통해서 새로운 세션ID를
+		// 발급해준다. 이렇게 처리하여 세션을 초기화
 //		rttr.addFlashAttribute("msg", "logout");
 		model.addAttribute("msg", "logout");
 		return "member/home";
 	}
-	
+
 	@RequestMapping("/list")
-	public String selectList(Model model
-			, @RequestParam(value = "searchOption", defaultValue = "all") String searchOption
-			, @RequestParam(value = "keyword", defaultValue = "") String keyword) {
+	public String selectList(PagingVO pvo, Model model,
+			@RequestParam(value = "nowPage", required = false) String nowPage,
+			@RequestParam(value = "cntPerPage", required = false) String cntPerPage,
+			@RequestParam(value = "searchOption", defaultValue = "all") String searchOption,
+			@RequestParam(value = "keyword", defaultValue = "") String keyword) {
+
+		// 게시물 글 총갯수
+		int total = sqlSession.selectOne("usermapper.countUser");
+
+		System.out.println("total: "+ total);
+		
+		if (nowPage == null && cntPerPage == null) {
+			nowPage = "1";
+			cntPerPage = "10";
+		} else if (nowPage == null) {
+			nowPage = "1";
+		} else if (cntPerPage == null) { 
+			cntPerPage = "10";
+		}
+		
+		// pvo 객체 생성
+		pvo = new PagingVO(total, Integer.parseInt(nowPage), Integer.parseInt(cntPerPage));
+		
+		int start = pvo.getStart();
+		int end = pvo.getEnd();
 		
 		Map<String, Object> parammap = new HashMap<String, Object>();
+
+		parammap.put("start", start);
+		parammap.put("end", end);
 		
 		parammap.put("searchOption", searchOption);
 		parammap.put("keyword", keyword);
+
+		List<UserVO> selectAll = sqlSession.selectList("usermapper.select", parammap);
 		
-		List<UserVO> selectAll = sqlSession.selectList("usermapper.select",parammap);
+		model.addAttribute("paging", pvo);
+		System.out.println("pvo : "+pvo);
+		
 		model.addAttribute("selectAll", selectAll);
-		
-		System.out.println("selectAll: "+ selectAll);
-		
+		System.out.println("selectAll: " + selectAll);
+
+
 		return "member/list";
 	}
-	
-	
+
 	@RequestMapping("/join")
 	public String join() {
-		
+
 		return "member/join";
 	}
-	
+
 	@RequestMapping("/insert")
 	public String insert(UserVO uvo) throws IOException {
-		
-		System.out.println("before insert uvo: "+uvo);
+
+		System.out.println("before insert uvo: " + uvo);
 		String fileName = null;
 		MultipartFile uploadFile = uvo.getUploadFile();
-		if(!uploadFile.isEmpty()) {
+		if (!uploadFile.isEmpty()) {
 			String originalFileName = uploadFile.getOriginalFilename();
-			String ext = FilenameUtils.getExtension(originalFileName);	// 확장자 구하기
-			UUID uuid = UUID.randomUUID();	// UUID 구하기
-			fileName = uuid+"."+ext;
-			uploadFile.transferTo(new File("I:\\upload\\"+fileName));
+			String ext = FilenameUtils.getExtension(originalFileName); // 확장자 구하기
+			UUID uuid = UUID.randomUUID(); // UUID 구하기
+			fileName = uuid + "." + ext;
+			uploadFile.transferTo(new File("I:\\upload\\" + fileName));
 		}
-		System.out.println("fileName: "+fileName);
+		System.out.println("fileName: " + fileName);
 		uvo.setFileName(fileName);
-		System.out.println("after insert uvo: "+uvo);
+		System.out.println("after insert uvo: " + uvo);
 		sqlSession.insert("usermapper.insert", uvo);
-		
+
 		return "redirect:list";
 	}
-	
+
 	@RequestMapping("/view")
 	public String view(@RequestParam("id") String id, Model model) {
-		
+
 		UserVO viewOne = sqlSession.selectOne("usermapper.view", id);
-		
-		System.out.println("viewOne: "+ viewOne);
-		
+
+		System.out.println("viewOne: " + viewOne);
+
 		model.addAttribute("viewOne", viewOne);
-		
+
 		return "member/view";
 	}
-	
+
 	@RequestMapping("/modify")
 	public String modify(UserVO uvo) {
-		
+
 		sqlSession.update("usermapper.modify", uvo);
-		System.out.println("update_uvo: "+ uvo);
-		
+		System.out.println("update_uvo: " + uvo);
+
 		return "redirect:list";
 	}
-	
-	
+
 	@RequestMapping("/delete")
 	public String delete(@RequestParam("id") String id) {
-		
+
 		sqlSession.delete("usermapper.delete", id);
 		System.out.println("delete 되었음...");
-		
+
 		return "redirect:list";
 	}
-	
+
 	// id 중복체크
 	@ResponseBody
 	@RequestMapping("/idcheck")
 	public String idcheck(HttpServletRequest request) {
-		
-		String id = request.getParameter("id").trim();	// 중간에 공백은 제거 못해! ex. h ong => hong (x)
-		
+
+		String id = request.getParameter("id").trim(); // 중간에 공백은 제거 못해! ex. h ong => hong (x)
+
 		int resultCount = sqlSession.selectOne("usermapper.idcheck", id);
-		
-		System.out.println("idcheck 되었음...id:"+id);
-		
+
+		System.out.println("idcheck 되었음...id:" + id);
+
 		String result = String.valueOf(resultCount);
-		
-		System.out.println("idcheck_result :"+result);
-		
+
+		System.out.println("idcheck_result :" + result);
+
 		return result;
 	}
-	
-	
-	
+
 }
--- src/main/webapp/WEB-INF/views/member/list.jsp
+++ src/main/webapp/WEB-INF/views/member/list.jsp
@@ -7,12 +7,33 @@
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
+<script type="text/javascript">
+	function selChange() {
+		 var sel = document.getElementById('cntPerPage').value;
+		
+		 location.href="list.do?nowPage=${paging.nowPage}&cntPerPage="+sel;
+	}
+
+</script> 
 </head>
 <body>
 <h1>list</h1>
 
 <%@ include file="/WEB-INF/views/include/header.jsp" %>
 
+ 	<div style="float: right;">
+		<select id="cntPerPage" name="sel" onchange="selChange()">
+			<option value="10"
+				<c:if test="${paging.cntPerPage == 10}">selected</c:if>>10줄 보기</option>
+			<option value="20"
+				<c:if test="${paging.cntPerPage == 20}">selected</c:if>>20줄 보기</option>
+			<option value="30"
+				<c:if test="${paging.cntPerPage == 30}">selected</c:if>>30줄 보기</option>
+			<option value="50"
+				<c:if test="${paging.cntPerPage == 50}">selected</c:if>>50줄 보기</option>
+		</select>
+	</div> <!-- 옵션선택 끝 -->
+	
 	<form align="left" name="form11" method="post" action="/member/list" ">
 		<select name="searchOption">
 			<option value="all" <c:if test="${map.searchOption == 'all'}">selected</c:if>>id+pwd+name</option>
@@ -25,30 +46,49 @@
 		<input type="submit" value="조회"/>
 	</form>	
 
-<table border="1">
-	<thead >
-	<tr>
-		<th>id</th>
-		<th>pwd</th>
-		<th>name</th>
-		<th>email</th>
-		<th>file</th>
-		<th>joinDate</th>
-	</tr>
-	</thead>
-	<tbody>
-	<c:forEach var="uvo" items="${selectAll}">
+	
+	<table border="1">
+		<thead >
 		<tr>
-			<td>${uvo.id}</td>
-			<td>${uvo.pwd}</td>
-			<td><a href="/member/view?id=${uvo.id}">${uvo.name}</a></td>
-			<td>${uvo.email}</td>
-			<td>${uvo.fileName}</td>
-			<td><fmt:formatDate value="${uvo.joindate}" pattern="yyyy-MM-dd HH:mm:ss"/></td>
+			<th>id</th>
+			<th>pwd</th>
+			<th>name</th>
+			<th>email</th>
+			<th width="30%">file</th>
+			<th>joinDate</th>
 		</tr>
-	</c:forEach>
-	</tbody>
-</table>
+		</thead>
+		<tbody>
+		<c:forEach var="uvo" items="${selectAll}">
+			<tr>
+				<td>${uvo.id}</td>
+				<td>${uvo.pwd}</td>
+				<td><a href="/member/view?id=${uvo.id}">${uvo.name}</a></td>
+				<td>${uvo.email}</td>
+				<td>${uvo.fileName}</td>
+				<td><fmt:formatDate value="${uvo.joindate}" pattern="yyyy-MM-dd HH:mm:ss"/></td>
+			</tr>
+		</c:forEach>
+		</tbody>
+	</table>
 
+	<div style="display: block; text-align: center;">		
+		<c:if test="${paging.startPage != 1 }">
+			<a href="${contextPath}/member/list?nowPage=${paging.startPage - 1 }&cntPerPage=${paging.cntPerPage}">[이전]</a> <!-- < -->
+		</c:if>
+		<c:forEach begin="${paging.startPage }" end="${paging.endPage }" var="p">
+			<c:choose>
+				<c:when test="${p == paging.nowPage }">
+					<b>${p }</b>
+				</c:when>
+				<c:when test="${p != paging.nowPage }">
+					<a href="${contextPath}/member/list?nowPage=${p }&cntPerPage=${paging.cntPerPage}">${p }</a>
+				</c:when>
+			</c:choose>
+		</c:forEach>
+		<c:if test="${paging.endPage != paging.lastPage}">
+			<a href="${contextPath}/member/list?nowPage=${paging.endPage+1 }&cntPerPage=${paging.cntPerPage}">[다음]</a><!-- > -->
+		</c:if>
+	</div>
 </body>
 </html>
